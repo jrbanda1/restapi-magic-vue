72814af48584b762a2c0b041e3b60a37cb841d86
jrbanda1 <jrbanda1@stu.southtexascollege.edu>
2023-10-08 15:58:34 -0500

    edit to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue-restapi-magic/',
+  base: '/restapi-magic-vue/',
   plugins: [vue()],
   resolve: {
     alias: {
